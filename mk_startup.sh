165836db400b0839ef83b43e674834a80dcfce7f
eukaryote <eukaryote31@gmail.com>
2019-04-15 22:23:23 -0600

    rocm stuff

--- mk_startup.sh
+++ mk_startup.sh
@@ -1,3 +1,6 @@
 echo Setup autostart
 mkdir -p ~/.config/autostart/
 cp -v desktop/* ~/.config/autostart/
+
+# omniboard
+(crontab -l 2>/dev/null; echo "@reboot bash -c 'while ! nc -z localhost 27017; do sleep 1; done && /home/eukaryote/.nvm/versions/node/v11.11.0/bin/omniboard -m localhost:27017:experiments'") | crontab -
